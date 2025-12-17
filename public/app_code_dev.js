1910279818927fb8bf2a318c59ca1c216528bbc4
wilzm <159954493+wilzm@users.noreply.github.com>
2025-12-16 21:21:57 -0500

    Update app_code_dev.js

--- public/app_code_dev.js
+++ public/app_code_dev.js
@@ -1,22 +1,16 @@
 var updateView = async (button) => {
+    let api = '';
 
     if (button.dataset.querytype == 'by_name') {
         let queryvalue = document.querySelector('#nameQuery').value;
-        api = `http://localhost:3000/api/by_name/${queryvalue}`;
+        api = `/api/by_name/${queryvalue}`; 
     } else if (button.dataset.querytype == 'by_age') {
         let queryStartAge = document.querySelector('#startAgeQuery').value;
         let queryEndAge = document.querySelector('#endAgeQuery').value;
-        api = `http://localhost:3000/api/by_age/${queryStartAge}/${queryEndAge}`;
+        api = `/api/by_age/${queryStartAge}/${queryEndAge}`; 
     }
+
     const data = await fetch(api);
     const model = await data.json();
     render_view(model);
-}
-
-var render_view = (model) => {
-    var source = document.querySelector("#show_results_view").innerHTML;
-    var template = Handlebars.compile(source);
-    var html = template(model);
-
-    document.querySelector("#results").innerHTML = html;
 }
